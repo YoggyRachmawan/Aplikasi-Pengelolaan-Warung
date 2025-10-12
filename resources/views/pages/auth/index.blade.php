3e70c40dd2ef47d92be8d663549e5b27de58c582
Yoggy Rachmawan <yoggyrachmawan45@gmail.com>
2025-10-12 19:55:28 +0700

    mengubah pada href

--- resources/views/pages/auth/index.blade.php
+++ resources/views/pages/auth/index.blade.php
@@ -7,13 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <link rel="shortcut icon" href="{{ secure_asset('assets/img/logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('assets/img/logo.png') }}" type="image/x-icon">
     <title>WarungKu</title>
-    <link href="{{ secure_asset('assets/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{asset('assets/css/styles.css') }}" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <style>
         .bg {
-            background-image: url("{{ secure_asset('assets/img/warung.jpg') }}");
+            background-image: url("{{asset('assets/img/warung.jpg') }}");
             height: 100%;
             background-position: center;
             background-repeat: no-repeat;
@@ -78,7 +78,7 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
-    <script src="{{ secure_asset('assets/js/scripts.js') }}"></script>
+    <script src="{{asset('assets/js/scripts.js') }}"></script>
     {{-- jquery --}}
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     {{-- sweetalert2 --}}
